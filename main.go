6d861218f2d4076794984f7426ddea4244699075
Donnie Tolbert <donniet@gmail.com>
2019-09-18 15:17:48 -0500

    added flags

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"flag"
+	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"strings"
 	tf_core_framework "tensorflow/core/framework"
@@ -19,13 +24,35 @@ import (
 
 var (
 	throttle                     = 1000 * time.Millisecond
-	motionURL                    = "http://mirror.local:8888/motion.bin"
 	processMotionAddress         = "localhost:8500"
+	modelName                    = "process_motion"
+	signatureName                = "serving_default"
+	modelVersion         int64   = 1
+	inputName                    = "input"
+	outputName                   = "output"
 	totalAverageMotion   float32 = 5.
 	motionGRPCAddress            = "mirror.local:5555"
 	wakupAddress                 = "http://mirror.local:9080/"
 	wakupThrottle                = 10 * time.Second
+	mbx                          = 104
+	mby                          = 78
 )
+
+func init() {
+	flag.DurationVar(&throttle, "throttle", throttle, "throttle motion detection to save CPU")
+	flag.DurationVar(&wakupThrottle, "wakeupThrottle", wakupThrottle, "minimum time before wakup signals are sent")
+	flag.StringVar(&processMotionAddress, "servinggrpc", processMotionAddress, "GRPC address to TensorFlow Serving for Motion Processing")
+	flag.StringVar(&modelName, "model", modelName, "TensorFlow Serving Model Name")
+	flag.StringVar(&signatureName, "signature", signatureName, "TensorFlow Serving Model Signature Name")
+	flag.Int64Var(&modelVersion, "modelversion", modelVersion, "TensorFlow Serving Model Version")
+	flag.StringVar(&inputName, "modelinput", inputName, "TensorFlow Serving Model Input Name")
+	flag.StringVar(&outputName, "modeloutput", outputName, "TensorFlow Serving Model Output Name")
+	flag.StringVar(&motionGRPCAddress, "motiongrpc", motionGRPCAddress, "GRPC address of motion data")
+	flag.StringVar(&modelName, "modelName", modelName, "name of motion processing model in tensorflow serving")
+	flag.IntVar(&mbx, "mbx", mbx, "macro blocks in X direction")
+	flag.IntVar(&mby, "mby", mby, "macro blocks in y direction")
+
+}
 
 type motionVector struct {
 	X   int8
@@ -33,40 +60,46 @@ type motionVector struct {
 	Sad int16
 }
 
-func calculateMotionFromVectors(predictClient pb.PredictionServiceClient, body []byte) (float32, error) {
-	// log.Printf("len(body): %d", len(body))
+func serialCalculateMotionFroMVectors(body []byte, mbx, mby int) (float64, error) {
+	log.Printf("len(body): %d", len(body))
 
-	// vect := make([]motionVector, 104*78)
+	vect := make([]motionVector, mbx*mby)
 
-	// err := binary.Read(bytes.NewReader(body), binary.LittleEndian, vect)
+	if err := binary.Read(bytes.NewReader(body), binary.LittleEndian, vect); err != nil {
+		return 0., err
+	}
 
-	// tot := 0.
-	// for i := 0; i < 104*78; i++ {
-	// 	x := float64(vect[i].X)
-	// 	y := float64(vect[i].Y)
-	// 	mag2 := x*x + y*y
+	tot := 0.
+	for i := 0; i < mbx*mby; i++ {
+		x := float64(vect[i].X)
+		y := float64(vect[i].Y)
+		mag2 := x*x + y*y
 
-	// 	tot += math.Sqrt(mag2)
-	// }
-	// tot /= (104 * 78)
-	// fmt.Printf("manually calculating total mag: %f\n", tot)
+		tot += math.Sqrt(mag2)
+	}
+	tot /= float64(mbx * mby)
+	fmt.Printf("manually calculating total mag: %f\n", tot)
 
+	return tot, nil
+}
+
+func calculateMotionFromVectors(predictClient pb.PredictionServiceClient, body []byte, mbx, mby int) (float32, error) {
 	request := &pb.PredictRequest{
 		ModelSpec: &pb.ModelSpec{
-			Name:          "process_motion",
-			SignatureName: "serving_default",
+			Name:          modelName,
+			SignatureName: signatureName,
 			Version: &google_protobuf.Int64Value{
-				Value: int64(1),
+				Value: modelVersion,
 			},
 		},
 		Inputs: map[string]*tf_core_framework.TensorProto{
-			"input": &tf_core_framework.TensorProto{
+			inputName: &tf_core_framework.TensorProto{
 				Dtype: tf_core_framework.DataType_DT_INT8,
 				TensorShape: &tf_core_framework.TensorShapeProto{
 					Dim: []*tf_core_framework.TensorShapeProto_Dim{
 						&tf_core_framework.TensorShapeProto_Dim{Size: int64(1)},
-						&tf_core_framework.TensorShapeProto_Dim{Size: int64(104)},
-						&tf_core_framework.TensorShapeProto_Dim{Size: int64(78)},
+						&tf_core_framework.TensorShapeProto_Dim{Size: int64(mbx)},
+						&tf_core_framework.TensorShapeProto_Dim{Size: int64(mby)},
 						&tf_core_framework.TensorShapeProto_Dim{Size: int64(4)},
 					},
 				},
@@ -80,10 +113,16 @@ func calculateMotionFromVectors(predictClient pb.PredictionServiceClient, body [
 		return 0, err
 	}
 
-	return res.Outputs["output"].GetFloatVal()[0], nil
+	o, ok := res.Outputs[outputName]
+	if !ok {
+		return 0, fmt.Errorf("output '%s' not found", outputName)
+	}
+	return o.GetFloatVal()[0], nil
 }
 
 func main() {
+	flag.Parse()
+
 	conn, err := grpc.Dial(processMotionAddress, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("error connecting to serving: %v", err)
@@ -117,7 +156,7 @@ func main() {
 
 		body := frame.GetData()
 
-		if motion, err := calculateMotionFromVectors(predictClient, body); err != nil {
+		if motion, err := calculateMotionFromVectors(predictClient, body, mbx, mby); err != nil {
 			log.Printf("error calculating motion: %v", err)
 		} else if motion > totalAverageMotion {
 			if time.Now().Sub(lastWakeup) > wakupThrottle {
@@ -129,22 +168,7 @@ func main() {
 			}
 		}
 
-		// res, err := http.Get(motionURL)
-		// if err != nil {
-		// 	log.Printf("error retrieving motion data from '%s': %v", motionURL, err)
-		// } else if res.StatusCode != http.StatusOK {
-		// 	body, _ := ioutil.ReadAll(res.Body)
-
-		// 	log.Printf("non-ok status (%s) code from url: '%s': %s", res.Status, motionURL, string(body))
-		// } else if body, err := ioutil.ReadAll(res.Body); err != nil {
-		// 	log.Printf("error reading body: %v", err)
-		// } else if motion, err := calculateMotionFromVectors(predictClient, body); err != nil {
-		// 	log.Printf("error calculating motion: %v", err)
-		// } else {
-		// 	fmt.Printf("*** MOTION: %f\n", motion)
-		// }
-
-		if s := time.Now().Sub(t); s > 0 {
+		if s := time.Now().Sub(t); s > throttle {
 			time.Sleep(s)
 		}
 	}
